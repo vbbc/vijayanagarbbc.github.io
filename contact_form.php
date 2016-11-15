3645ec72baff265b3acb966cf3ffc27b7ec6b675
suj009 <suj009@gmail.com>
2016-11-15 09:10:37 +0530

    save
    s

--- contact_form.php
+++ contact_form.php
@@ -1,10 +1,9 @@
 <?php
-	$msg= 'Name: ' .$_POST['fname']. "\n"
-		  .'ContactNo: ' .$_POST['contactno']. "\n"
-		  .'Email: ' .$_POST['email']. "\n"
-		  .'TeamName: ' .$_POST['teamname']. "\n"
-		  . 'Message: ' .$_POST['message'];
-	mail('vijayanagarbbc@gmail.com' , 'contact us form' , $msg);
+	$msg= 'Contact Person Name: ' .$_POST['fname']. "\n"
+		  .'Contact Number: ' .$_POST['contactno']. "\n"
+		  .'Team Email address: ' .$_POST['email']. "\n"
+		  .'Team Name: ' .$_POST['teamname']. "\n"
+		  . 'Team Members: ' .$_POST['message'];
+	mail('vijayanagarbbc@gmail.com' , 'Tournament Entry Team Information' , $msg);
 	header( 'Location: http://vijaynagarbbc.com/contact.php' ) ;
 ?>
-		  
